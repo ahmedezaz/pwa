87bcb05f0f77240b3c953ef627459c7655dc8522
Ezaz Ahmed <ahmedezaz@users.noreply.github.com>
2022-05-15 22:51:05 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,21 +12,29 @@ self.addEventListener('install', (e) => {
   );
 });
 
-self.addEventListener('fetch', function (e) {
-  console.log('Start Service Worker');
-  e.respondWith(
-    (async () => {
-      const res = await caches.match(e.request);
-      console.log(`Service Worker Fetching: ${e.request.url}`);
-      if (res) {
-        return res;
-      }
-      const response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`Service Worker Caching new files: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })()
+// self.addEventListener('fetch', function (e) {
+//   console.log('Start Service Worker');
+//   e.respondWith(
+//     (async () => {
+//       const res = await caches.match(e.request);
+//       console.log(`Service Worker Fetching: ${e.request.url}`);
+//       if (res) {
+//         return res;
+//       }
+//       const response = await fetch(e.request);
+//       const cache = await caches.open(cacheName);
+//       console.log(`Service Worker Caching new files: ${e.request.url}`);
+//       cache.put(e.request, response.clone());
+//       return response;
+//     })()
+//   );
+// });
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
 
